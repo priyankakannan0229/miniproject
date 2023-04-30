73bc43a61651dd4d03671b61a1747542df6e5be1
MADHAVAN5 <kmadavan018@gmail.com>
2023-04-30 12:24:51 +0530

    mongo DB config

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 import sys
 from flask import Flask
+import pymongo
 
 STATIC_FOLDER = sys.path[0] + '/static/'
 TEMPLATES_FOLDER = sys.path[0] + '/templates/'
 app = Flask(__name__, template_folder=TEMPLATES_FOLDER, static_folder=STATIC_FOLDER)
+
+app.config['MONGO_URI'] = 'mongodb+srv://priyankakannan:ammu0229@cluster0.h1g8883.mongodb.net/?retryWrites=true&w=majority'
+app.secret_key = "SSKEY"
+
+client = pymongo.MongoClient("mongodb+srv://priyankakannan:ammu0229@cluster0.h1g8883.mongodb.net/?retryWrites=true&w=majority")
+db = client.complaint
--- views/views.py
+++ views/views.py
@@ -4,3 +4,7 @@ from app import *
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
