5e617549a17719fb4010fe55b143c6b7c5881196
MADHAVAN5 <kmadavan018@gmail.com>
2023-05-04 15:18:33 +0530

    assignment 2 submitted

--- views/views.py
+++ views/views.py
@@ -1,14 +1,76 @@
 from flask import make_response,Flask, flash, redirect, render_template, request, url_for, session
 from app import *
+import bcrypt
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+#Login
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect('/dashboard')
 
-@app.route('/register')
-def register():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+       
+        email_found = db.user.find_one({"email": email})
+        if email_found:
+            email_val = email_found['email']
+            passwordcheck = email_found['password']
+            
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["email"] = email_val
+                return redirect('/dashboard')
+            else:
+                if "email" in session:
+                    return redirect(url_for("logged_in"))
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
+        else:
+            message = 'Email not found'
+            return render_template('login.html', message=message)
+
+    return render_template('login.html', message=message)
+
+#SignUp
+@app.route('/register', methods=["POST", "GET"])
+def signup():
+    message = ''
+    if "email" in session:
+        return redirect('/dashboard')
+
+    if request.method == "POST":
+        user = request.form.get("name")
+        email = request.form.get("email")
+        
+        password1 = request.form.get("password")
+        password2 = request.form.get("cpassword")
+        phno = request.form.get("phno")
+
+        email_found = db.user.find_one({"email": email})
+        
+        if email_found:
+            message = 'This email already exists in database'
+            return render_template('login.html', message=message)
+        if password1 != password2:
+            message = 'Passwords should match!'
+            return render_template('login.html', message=message)
+
+        else:
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            user_input = {'name': user, 'email': email, 'password': hashed, 'phoneNo':phno}
+            db.user.insert_one(user_input)
+            
+            user_data = db.user.find_one({"email": email})
+            new_email = user_data['email']
+   
+            return redirect('/dashboard')
+
     return render_template('register.html')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
