e2227ac5d9c841b7a8de37265684baac949c47aa
MADHAVAN5 <kmadavan018@gmail.com>
2023-05-07 15:14:13 +0530

    Authentication || mongo DB

--- views/views.py
+++ views/views.py
@@ -11,24 +11,36 @@ def home():
 def login():
     message = 'Please login to your account'
     if "email" in session:
-        return redirect('/dashboard')
+        user = db.user.find_one({'email':session['email']})
+        type = user['type']
+        if type == 'user':
+            return redirect('/user')
+        else:
+            return redirect('/dashboard')
 
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        print(email)
+        type = request.form.get("type")
         email_found = db.user.find_one({"email": email})
         if email_found:
             email_val = email_found['email']
             passwordcheck = email_found['password']
-            
-            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
-                session["email"] = email_val
-                return redirect('/dashboard')
+            user_type = email_found['type']
+            if type == user_type:
+                if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                    session["email"] = email_val
+                    if type == 'user':
+                        return redirect('/user')
+                    else:
+                        return redirect('/dashboard')
+                else:
+                    if "email" in session:
+                        return redirect(url_for("logged_in"))
+                    message = 'Wrong password'
+                    return render_template('login.html', message=message)
             else:
-                if "email" in session:
-                    return redirect(url_for("logged_in"))
-                message = 'Wrong password'
+                message = "Type invalid"
                 return render_template('login.html', message=message)
         else:
             message = 'Email not found'
@@ -48,27 +60,23 @@ def signup():
         email = request.form.get("email")
         password1 = request.form.get("password")
         password2 = request.form.get("cpassword")
-        print(password1)
-        print(password2)
         phno = request.form.get("phno")
         type = request.form.get("type")
-        print(type)
 
         email_found = db.user.find_one({"email": email})
         
         if email_found:
             message = 'This email already exists in database'
             return render_template('login.html', message=message)
-            print(message)
         
         if password1 != password2:
             message = 'Passwords should match!'
-            print(message)
+            return render_template('register.html', message=message)
 
         else:
             hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
-            # user_input = {'name': user, 'email': email, 'password': hashed, 'phoneNo':phno, 'type':type}
-            # db.user.insert_one(user_input)
+            user_input = {'name': user, 'email': email, 'password': hashed, 'phoneNo':phno, 'type':type}
+            db.user.insert_one(user_input)
             
             user_data = db.user.find_one({"email": email})
             new_email = user_data['email']
@@ -77,6 +85,27 @@ def signup():
 
     return render_template('register.html')
 
+#logout
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        return redirect('/login')
+    else:
+        return redirect('/login')
+
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    if "email" in session:
+        user = db.user.find({'email':session['email']})   
+        return render_template('dashboard.html')
+    else:
+        return redirect('/login')
+
+@app.route('/user')    
+def user():
+    if "email" in session:
+        user = db.user.find({'email':session['email']})   
+        return render_template('user.html')
+    else:
+        return redirect('/login') 
